52d2bb9d76cb10d75aecf69910b38dae92827fe2
DBENSAN <dftb97@gmail.com>
2022-12-30 17:54:30 -0800

    Added html base

---
+++ assets/script.js
@@ -0,0 +1,107 @@
+var quizBank = [
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+    
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+
+    {
+        question: "",
+        choices: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        answer: "b"
+    },
+]
+
+var score = 0;
+
+function weeWoo() {
+    
+}
